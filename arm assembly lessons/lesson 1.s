682518969126e62b80402f37d5416f3dbf982835
Diegomendes0706 <diego.mendesgarcia@gmail.com>
2024-11-07 16:47:27 -0300

    ADD, SUB, MUL, set CPSR

--- arm assembly lessons/lesson 1.s
+++ arm assembly lessons/lesson 1.s
@@ -1,5 +1,5 @@
 .global _start
 _start:
-		mov r0 , #42
-		mov r7, #1
-		swi 0
+	mov r0 , #42
+	mov r7, #1
+	swi 0
---
+++ arm assembly lessons/lesson 2.s
@@ -0,0 +1,6 @@
+.global _start
+_start:
+	mov r0, #3
+	mov r1, #3
+	
+	subs r2, r0, r1
